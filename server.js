bbee2ffd2c6b5126646d3a948513cc0b120bcbec
System Administrator <root@Nasimjons-MacBook-Pro.local>
2019-03-10 08:38:19 +0500

    Change index.js and server.js

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var http = require("http");
-function start(){
-function onRequest(request, response) {
-  console.log("Request received.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+var url = require('url');
+
+
+
+function start(route, handle){
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for" + pathname + "received.")
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    var content = route(handle, pathname);
+    response.write(content);
+    response.end();
 }
 
 http.createServer(onRequest).listen(8888);
-
 console.log("Server has started.");
 
 }
